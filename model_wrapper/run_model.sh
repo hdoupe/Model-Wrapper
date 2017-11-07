5a91b24a2a0e57e63e36d307f07ce1ce3fe01846
Henry Doupe <hdoupe@yahoo.com>
2017-11-07 17:44:20 -0500

    Update run_model.sh for nohup logging

--- model_wrapper/run_model.sh
+++ model_wrapper/run_model.sh
@@ -2,4 +2,4 @@
 export PATH="/home/ubuntu/miniconda3/bin:$PATH"
 cd ~/OG-USA/regression
 source activate ospcdyn
-python run_reg_reforms.py --use_cps --cpu_count=4 > output.txt
+python run_reg_reforms.py --use_cps --cpu_count=4
